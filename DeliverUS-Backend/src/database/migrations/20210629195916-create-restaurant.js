326e2619bb434834a635e2c236dfaee088f41864
Roberto <robservil@alumn.us.es>
2024-07-02 00:52:37 +0200

    prueba

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -1,13 +1,100 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Restaurants', {
-      id: {
+      id: { // Si queremos permitir que sea nulo no hace falta poner allowNull: true
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      postalCode: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      shippingCosts: {
+        type: Sequelize.DOUBLE,
+        defaultValue: 0.0,
+        allowNull: false
+      },
+      averageServiceMinutes: {
+        type: Sequelize.DOUBLE,
+        allowNull: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      logo: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      heroImage: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values: [
+          'online',
+          'offline',
+          'closed',
+          'temporarily closed'
+        ],
+        allowNull: false,
+        defaultValue: 'offline'
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: new Date()
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: {
+            tableName: 'Users'
+          },
+          key: 'id'
+        }
+      },
+      restaurantCategoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'RestaurantCategories'
+          },
+          key: 'id'
+        }
       }
-      // TODO: Include the rest of the fields of the Restaurants table
+      // TODO: Include the rest of the fields of the Restaurants table   // DONE
 
     })
   },
